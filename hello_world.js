eb656ef89f15d3935885fcf2c8c5dbed60312bc4
teamoo24 <601107w@gmail.com>
2020-05-02 15:24:38 +0900

    ジョークを読み込むことができる

--- hello_world.js
+++ hello_world.js
@@ -2,4 +2,10 @@ console.log('hello world!');
 
 // node.jsでは、デフォルトで多様なモジュールが提供されており、JavaScriptからは「require」で読み込むことができます。
 const path = require('path');
-console.log('')
+console.log(path.basename(__filename));
+
+// 上の様な感じでモジュールを利用してジョークを読み込む
+var giveMeJoke = require('give-me-a-joke');
+giveMeJoke.getRandomDadJoke(function(joke) {
+	console.log(joke)
+});
