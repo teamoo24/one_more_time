1290d68da9facbeb63d2a53e400a7bbf4145b658
teamoo24 <601107w@gmail.com>
2020-05-03 22:35:15 +0900

    サーバー構築

---
+++ src/deps.ts
@@ -0,0 +1,3 @@
+export default function sum(v1: number, v2: number):number {
+	return v1+v2;
+}
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import sum from './deps'
+
+const n = 1;
+const s = 2;
+
+const result = sum(n, s);
+console.log(result)
